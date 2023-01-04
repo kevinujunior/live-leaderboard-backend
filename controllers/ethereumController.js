a03b5edbf0ee6dfb324641aa742c2ec3e2ddb19b
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2023-01-04 12:08:55 +0530

    Update ethereumController.js

--- controllers/ethereumController.js
+++ controllers/ethereumController.js
@@ -1,43 +1,42 @@
 //There will be two actions users can do - deposit ethereum and withdraw ethereum
 
 
-/*
-Purpose :  To deposit ethereum
-Input : Amount to be deposited
-*/
-export async function ethereumDeposit(req, res, next) {
-    try {
-        const score = req.body.score
-        const currUser = req.user
-        currUser.eth = currUser.eth + score
+//Here we have used key-value export 
+module.exports = {
 
-        await currUser.save()
+    //function to handle ethereum deposit requests
+    ethereumDeposit : async(req,res,next)  => {
+        try{
+            const score = req.body.score
+            const currUser = req.user
+            currUser.eth = currUser.eth + score
 
-        return res.status(200).json({ Message: score + " Ethereum Deposited " })
+            await currUser.save()
+
+            return res.status(200).json({Message:score + " Ethereum Deposited "})
+        }
+        catch(err){
+            res.status(400).json({Message:"Invalid Request "+err})
+        }
+    },
+
+    //function to handle ethereum withdraw requests
+    ethereumWithdraw : async(req,res,next)  => {
+        try{
+
+            const score = req.body.score
+            const currUser = req.user
+            currUser.eth = currUser.eth - score
+
+            await currUser.save()
+
+            return res.status(200).json({Message:score + " Ethereum Withdrawn "})
+
+        }
+        catch(err){
+            res.status(400).json({Message:"Invalid Request "+err})
+        }
     }
-    catch (err) {
-        res.status(400).json({ Message: "Invalid Request " + err })
-    }
-}
 
-/*
-Purpose :  To withdraw ethereum
-Input : Amount to be withdrawn
-*/
 
-export async function ethereumWithdraw(req, res, next) {
-    try {
-
-        const score = req.body.score
-        const currUser = req.user
-        currUser.eth = currUser.eth - score
-
-        await currUser.save()
-
-        return res.status(200).json({ Message: score + " Ethereum Withdrawn " })
-
-    }
-    catch (err) {
-        res.status(400).json({ Message: "Invalid Request " + err })
-    }
 }
